f3ba79fa807b480d6ef0c51f6dfd47300a5b4e1b
Witold Sobczak <witold.sobczak@comarch.com>
2022-10-21 16:16:25 +0200

    [Init] Setup simple project

---
+++ default-auth-service/src/main/java/com/github/witosh/authorization/DefaultAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.witosh.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultAuthServiceApplication.class, args);
+	}
+
+}
---
+++ default-auth-service/src/test/java/com/github/witosh/authorization/DefaultAuthServiceApplicationTests.java
@@ -0,0 +1,10 @@
+package com.github.witosh.authorization;
+
+//@SpringBootTest
+class DefaultAuthServiceApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
